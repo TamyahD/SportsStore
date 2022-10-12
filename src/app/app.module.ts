ded99c9981ec628217ca9f995b39facabbdc824f
TamyahD <tamyahdownes@yahoo.com>
2022-10-12 15:26:20 -0400

    modified root module to integrate new modules & components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StoreModule } from './store/store.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    StoreModule,
     BrowserModule
   ],
   providers: [],
