2c9311e869fd848e3ed8305d594b278882c51df6
TamyahD <tamyahdownes@yahoo.com>
2022-10-26 15:10:34 -0400

    created & implemented route guard to prevent automatic navigation to admin features

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,15 +3,17 @@ import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
 import { AdminComponent } from "./admin.component";
+import { AuthGuard } from "./auth.guard";
 import { AuthComponent } from "./authorization.component";
 
 let routing = RouterModule.forChild([
     { path: "authorization", component: AuthComponent },
-    { path: "main", component: AdminComponent },
+    { path: "main", component: AdminComponent, canActivate: [AuthGuard] },
     { path: "**", redirectTo: "authorization" }
 ]);
 @NgModule({
     imports: [CommonModule, FormsModule, routing],
+    providers: [AuthGuard],
     declarations: [AuthComponent, AdminComponent]
 })
 export class AdminModule {}
